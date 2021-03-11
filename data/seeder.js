edf24bf4951996830a8e85d5acfdf347e3f623fd
ahmed mahmoud <ahmedm.elnewery@gmail.com>
2021-03-11 12:59:06 +0200

    import data

--- data/seeder.js
+++ data/seeder.js
@@ -1,16 +1,12 @@
+const dotenv = require("dotenv")
 const connectDB = require("../config/connectDB")
 const Product = require("../model/productModel")
 const products = require("./products")
 
+dotenv.config()
 connectDB()
 const importData =  () => {
-    // try {
-    //   await Product.insertMany(products)
-    //     process.exit(0)
-    // } catch (error) {
-    //   console.error("Error in importing Data", error)
-    //   process.exit(1)
-    // }
+  
     Product.insertMany(products,(err,data)=>{
         if(err){
             console.error("Error in importing Data")
