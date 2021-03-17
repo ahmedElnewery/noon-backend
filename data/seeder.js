a86482fccc4b9059708be43caebf4d76419dd63a
ahmed mahmoud <ahmedm.elnewery@gmail.com>
2021-03-17 12:39:42 +0200

    add images to db

--- data/seeder.js
+++ data/seeder.js
@@ -1,20 +1,53 @@
-const dotenv = require("dotenv")
-const connectDB = require("../config/connectDB")
-const Product = require("../model/product")
-const products = require("./products")
+const dotenv = require("dotenv");
+const connectDB = require("../config/connectDB");
+const Product = require("../model/product");
+const products = require("./products");
 
-dotenv.config()
-connectDB()
-const importData =  () => {
-  
-    Product.insertMany(products,(err,data)=>{
-        if(err){
-            console.error("Error in importing Data")
-           process.exit(1)
-        }else {
-            console.log("successfully import data")
-            process.exit(0)
-        }
-    })
-  }
-importData()
+dotenv.config();
+connectDB();
+
+const before = (subcategory) => {
+  const filtered = products.filter((p) => p.subcategory == subcategory);
+
+  filtered.forEach((pro, i) => {
+   
+        
+          const images = {
+            image: [
+              `../../../../assets/Img/products/${subcategory}/${i + 1}/1.jpg`,
+              `../../../../assets/Img/products/${subcategory}/${i + 1}/2.jpg`,
+              `../../../../assets/Img/products/${subcategory}/${i + 1}/3.jpg`,
+              `../../../../assets/Img/products/${subcategory}/${i + 1}/4.jpg`,
+            ],
+          };
+          pro.image = images.image;
+        
+
+  });
+};
+before("menWear");
+before("womenWear");
+before("kidWear");
+before("mobiles");
+before("labtops");
+before("tv");
+before("skinCare");
+before("hairCare");
+before("makeup");
+before("kitchen");
+before("furniture");
+before("homeDecor");
+
+
+function importData (){
+  Product.insertMany(products, (err, data) => {
+    if (err) {
+      console.error("Error in importing Data",err);
+      process.exit(1);
+    } else {
+      console.log("successfully import data");
+      process.exit(0);
+    }
+  });
+}
+importData();
